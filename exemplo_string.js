d94f6dffc8dc4d82d16c8e6cd42b71618a8eb4e6
Your Name <you@example.com>
2025-06-02 20:29:02 -0300

    Exemplo de manipulação de strings

---
+++ exemplo_string.js
@@ -0,0 +1,11 @@
+/*
+console.log("Este é exemplo de retorno no console");
+
+window.alert("Este é exemplo de retorno no navegador");
+*/
+
+const nomeCompleto = prompt("Digite seu nome completo:");
+const nome = nomeCompleto.split(' ');
+
+console.log('Tamanho do nome completo: ' + nomeCompleto.length);
+console.log('Nome:', nome);
