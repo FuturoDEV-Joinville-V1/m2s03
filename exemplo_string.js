cca16385ce5405ac966e74f3b0f7359d7144ae46
Your Name <you@example.com>
2025-06-02 20:32:37 -0300

    Exemplo I - Manipulação de string

--- exemplo_string.js
+++ exemplo_string.js
@@ -2,10 +2,12 @@
 console.log("Este é exemplo de retorno no console");
 
 window.alert("Este é exemplo de retorno no navegador");
+
+const variavel = prompt("Este é um exemplo de interação com o usuário via prompt:");
 */
 
 const nomeCompleto = prompt("Digite seu nome completo:");
 const nome = nomeCompleto.split(' ');
 
+console.log('Primeiro nome:', nome[0]);
 console.log('Tamanho do nome completo: ' + nomeCompleto.length);
-console.log('Nome:', nome);
