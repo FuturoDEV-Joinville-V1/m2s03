24f3b862b190c0b4e6ef9d6ae567881ef80fa6ae
Your Name <you@example.com>
2025-06-02 21:46:43 -0300

    [A01] Resolução Atividade 4

---
+++ atividade_4.js
@@ -0,0 +1,9 @@
+/*
+    Utilizando o prompt, crie um script que:
+    Receba um número do usuário;
+    Some 10 à esse número digitado pelo usuário e exiba o resultado no console do navegador;
+*/
+
+const numero = prompt("Digite um número:");
+
+console.log("Número digitado:", +numero + 10);
