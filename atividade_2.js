0c437bbe32a280455cad27f53e33265ffe20bee9
Your Name <you@example.com>
2025-06-02 21:23:58 -0300

    [A02] Resolução Atividade 2

---
+++ atividade_2.js
@@ -0,0 +1,10 @@
+/*
+ Crie uma variável chamada mensagem com o valor: “Eu amo programar em Java”
+ Substitua a palavra ‘Java’ por ‘JavaScript’ e exiba o resultado no console do navegador
+*/
+
+const fraseOrigial = 'Eu amo programar em Java';
+const fraseAtualizada = fraseOrigial.replace('Java','JavaScript');
+
+console.log('Frase origial:', fraseOrigial);
+console.log('Frase atualizada:', fraseAtualizada);
