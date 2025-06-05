1079fae3e4ccc9188cc637ef83bfed92c33212e3
Your Name <you@example.com>
2025-06-04 21:35:15 -0300

    [A02] Exemplo Ternário

---
+++ aula_2/exemplo_ternario.js
@@ -0,0 +1,17 @@
+/*
+    Receba dois números digitados pelo usuário (variáveis ‘a’ e ‘b’);
+
+    Compare os valores dessas variáveis e coloque no console:
+        Se a é maior que b: “a é maior que b”;
+        Se b é maior que a: “b é maior que a”;
+        Se a e b são iguais: “a e b são iguais”;
+*/
+
+const numero_1 = prompt("Digite o primeiro número:");
+const numero_2 = prompt("Digite o segundo número:");
+
++numero_1 > parseFloat(numero_2) ?
+    console.log('Primeiro número ' + numero_1 + ' é maior que o segundo número ' + numero_2) :
+    +numero_2 > +numero_1 ?
+    console.log(`Segundo número ${numero_2} é maior que o primeiro número ${numero_1}`) : 
+    console.log('Os números são iguais');
